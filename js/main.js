ad66c9c8e9acb57784b4518f8a072cb4eeac0222
Kevin Ezequiel OTERO <kevinotero09@hotmail.com>
2024-02-16 11:51:19 -0300

    tercer cambio

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 // Definimos una función para manejar el sistema bancario
 function sistemaBancario() {
-    // Inicializamos el saldo del usuario en 0
+    
     let saldo = 0;
-  
-    // Ciclo para realizar transacciones hasta que el usuario decida salir
+
     while (true) {
         // Mostramos las opciones disponibles al usuario
-        const opcion = prompt(`Bienvenido al Banco XYZ. Tu saldo actual es: ${saldo}\n\n¿Qué operación deseas realizar?\n1. Depositar\n2. Retirar\n3. Salir`);
-  
+        const opcion = prompt("Bienvenido al Banco Otero. Tu saldo actual es: ${saldo}\n\n¿Qué operación desea realizar?\n 1. Depositar\n 2. Retirar\n 3. Salir");
+
         // Verificamos la opción seleccionada por el usuario
         switch (opcion) {
             case '1':
@@ -20,7 +19,7 @@ function sistemaBancario() {
                     console.log("Cantidad inválida. Por favor, ingrese un número válido mayor que 0.");
                 }
                 break;
-  
+
             case '2':
                 // Retirar
                 const retiro = parseFloat(prompt("Ingrese la cantidad que desea retirar:"));
@@ -31,18 +30,18 @@ function sistemaBancario() {
                     console.log("Cantidad inválida o saldo insuficiente.");
                 }
                 break;
-  
+
             case '3':
                 // Salir
-                console.log("Gracias por utilizar nuestros servicios. ¡Hasta luego!");
+                console.log("Gracias por utilizar nuestros servicios.");
                 return;
-  
+
             default:
-                console.log("Opción inválida. Por favor, selecciona una opción válida.");
+                console.log("Opción inválida. Por favor, seleccione una opción válida.");
                 break;
         }
     }
-  }
-  
-  // Iniciamos el sistema bancario
-  sistemaBancario();
+}
+
+sistemaBancario();
+
